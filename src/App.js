39b6ae60a40f9b3f5c659249b705836607879a37
BB5014 <bblabriz@bbox.fr>
2019-12-09 15:26:34 +0100

    construction Routes and components : Formulaire, Message, NotFound

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+
+import Formulaire from "./components/Formulaire";
+import Message from "./components/Message";
 
 class App extends Component {
   render () {
     return (
-      <div className='box' />
-    )
+		<div className="box">
+			<div>
+				<div className="messages">
+					<Message />
+					<Message />
+					<Message />
+				</div>
+			</div>
+			<Formulaire />
+		</div>
+	);
   }
 }
 
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 class Connexion extends Component {
-    render() {
-        return (
-            <div className='connexionBox'>
-                <form className='connexion'>
-                    <input
-                        placeholder='Pseudo'
-                        type='text'
-                        required />
-                    <button type='submit'>GO</button>
-                </form>
+	state = {
+		pseudo: "",
+		goToChat: false
+	};
 
-            </div>
-        );
-    }
+	handleChange = event => {
+		const pseudo = event.target.value;
+		this.setState({ pseudo });
+	};
+
+	handleSubmit = event => {
+		// Gérer par soi-même l'event et non le navigateur
+		event.preventDefault();
+		this.setState({ goToChat: true });
+	};
+
+	render() {
+		if (this.state.goToChat) {
+			return <Redirect push to={`/pseudo/${this.state.pseudo}`}></Redirect>;
+		}
+		return (
+			<div className="connexionBox">
+				<form className="connexion" onSubmit={this.handleSubmit}>
+					<input
+						value={this.state.pseudo}
+						onChange={this.handleChange}
+						placeholder="Pseudo"
+						type="text"
+						required
+					/>
+					<button type="submit">GO</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Connexion;
