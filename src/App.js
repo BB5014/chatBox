919c4afe438ed778becef71bae558bbc8a35a589
BB5014 <bblabriz@bbox.fr>
2019-12-10 15:53:52 +0100

    differenciate users with conditional render  and file animation.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component, createRef } from "react";
 import "./App.css";
+import "./animation.css";
 
 import Formulaire from "./components/Formulaire";
 import Message from "./components/Message";
 
 // Firebase
 import base from "./components/base";
+
+// Animation
+import {
+  CSSTransition,
+  TransitionGroup
+} from "react-transition-group";
 
 class App extends Component {
 	state = {
@@ -25,10 +32,10 @@ class App extends Component {
 	}
 
 	// We have need to scroll when state updated, so it's necessary use componentDidMount
-  // Use current who do reference at div with ref={this.messagesRef} to recover
-  componentDidUpdate() {
-    const ref = this.messagesRef.current;
-    // Now is possible use ref in Js
+	// Use current who do reference at div with ref={this.messagesRef} to recover
+	componentDidUpdate() {
+		const ref = this.messagesRef.current;
+		// Now is possible use ref in Js
 		ref.scrollTop = ref.scrollHeight;
 	}
 
@@ -37,26 +44,46 @@ class App extends Component {
 		const messages = { ...this.state.messages };
 		// add message with key only (Date.now)
 		messages[`message-${Date.now()}`] = message;
+		// We want 10 messages displayed max in ths chatbox and in Firebase  (synchronisation)
+    Object
+      .keys(messages)
+      .slice(0, -10)
+      .forEach(key => {
+        messages[key] = null
+      })
 		// update state
 		this.setState({ messages });
-	};
+  };
+  
+  // Differentiate user
+  isUser =  pseudo => 
+    pseudo ===  this.state.pseudo
+  
 
 	render() {
 		// Display messages(state)
 
-		const displayMessages = Object.keys(this.state.messages).map(key => (
+    const displayMessages = Object.keys(this.state.messages).map(key => (
+      <CSSTransition
+        timeout={200}
+
+        // Special classNames with CSSTransition
+        classNames="fade"
+
+        key={key}>
 			<Message
-				key={key}
+				isUser={this.isUser}
 				message={this.state.messages[key].message}
 				pseudo={this.state.messages[key].pseudo}
-			></Message>
-		));
+			/>
+		</CSSTransition>
+	));
 		return (
 			<div className="box">
 				<div>
 					{/* I want scroll in bottom  in all the messages in this div so i put ref here */}
 					<div className="messages" ref={this.messagesRef}>
-						<div className="message">{displayMessages}</div>
+						<TransitionGroup className="message">{displayMessages}</TransitionGroup>
 					</div>
 				</div>
 				<Formulaire
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 // Display message content
 
-const Message = ({pseudo, message}) => {
-    return (
-        <p className="user-message">{message}</p>
-    )
+const Message = ({ pseudo, message, isUser }) => {
+    if (isUser(pseudo)) {
+
+        return (
+            <p className="user-message">{message}</p>
+        )
+    } else {
+        return (
+            <p className="not-user-message"><strong>{pseudo } : </strong>{message}</p>
+        )
+    }
+
 }
 
 export default Message;
